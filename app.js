cdbd324f44eb0f444020f23caaf38b8eb1ab8c79
Tobi Banks <tobibanks@yahoo.co.uk>
2021-03-08 23:51:11 +0000

    restarted, using fresh ideas. Comments indicate what to work on

---
+++ app.js
@@ -0,0 +1,20 @@
+const GameBoard = (function() {
+    const board = [];
+    // How to create interactivity?
+    // How will Player use the gameboard?
+    return {
+        gameBoard : function() {
+            board;
+        }
+    }
+})();
+
+const Player = function(name, marker) {
+    // what can a player do?
+    // what can a player influence?
+}
+
+const controlFlow = (function() {
+    // How to play the game? 
+    // What are the steps needed to progress the game further?
+})();
